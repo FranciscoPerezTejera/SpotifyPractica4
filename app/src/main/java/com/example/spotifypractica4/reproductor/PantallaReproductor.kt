8493bc24f3b4c153e632b7abc400e181eb3701e4
2damb <2damb@informatica.edu>
2023-12-19 17:01:15 +0000

    19/12/23: clase canción creada, canciones añadidas, clase exoplayerViewModel creada y retoques en pantallaReproductor

--- app/src/main/java/com/example/spotifypractica4/reproductor/PantallaReproductor.kt
+++ app/src/main/java/com/example/spotifypractica4/reproductor/PantallaReproductor.kt
@@ -1,16 +1,16 @@
 package com.example.spotifypractica4.reproductor
 
-import android.widget.Spinner
-import android.widget.SpinnerAdapter
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,18 +27,36 @@ fun PantallaReproductor() {
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween){
-        Text(text = "Album o título del disco", modifier = Modifier.fillMaxWidth().padding(8.dp))
+        Text(text = "Album o título del disco", modifier = Modifier
+            .align(Alignment.CenterHorizontally))
         Image(painter = painterResource(id = R.drawable.badbunny), contentDescription = "",
-            modifier = Modifier.fillMaxWidth().padding(8.dp).size(450.dp))
-        Text(text = "Título de la canción", modifier = Modifier.fillMaxWidth().padding(8.dp))
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .size(450.dp))
+        Text(text = "Título de la canción", modifier = Modifier
+            .align(Alignment.CenterHorizontally))
         Slider(value = 0f, onValueChange = {})
         Row (horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxWidth().padding(8.dp)) {
-            Button(onClick = { /*TODO*/ }) {}
-            Button(onClick = { /*TODO*/ }) {}
-            Button(onClick = { /*TODO*/ }) {}
-            Button(onClick = { /*TODO*/ }) {}
-            Button(onClick = { /*TODO*/ }) {}
+            modifier = Modifier.fillMaxWidth().padding(8.dp)){
+            Text(text = "0:00")
+            Text(text = "3:41")
+        }
+        Row (horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)) {
+
+            Button(onClick = { /*TODO*/ }) {
+            }
+            Button(onClick = { /*TODO*/ }) {
+            }
+            Button(onClick = { /*TODO*/ }) {
+            }
+            Button(onClick = { /*TODO*/ }) {
+            }
+            Button(onClick = { /*TODO*/ }) {
+            }
         }
     }
 
---
+++ app/src/main/java/com/example/spotifypractica4/viewmodel/ExoplayerViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.spotifypractica4.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ExoPlayerViewModel : ViewModel(){
+
+
+
+}
